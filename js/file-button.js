c6c41d898533b26eba8d03a9d65ef1d39d40822f
Shiho Iwase <iwase@qnote.co.jp>
2022-06-30 17:59:13 +0900

    [add] hoverの追加

--- js/file-button.js
+++ js/file-button.js
@@ -1,4 +1,4 @@
-$('.p-file-button__selection input').on('change', function () {
+$('.c-button__file--selection input').on('change', function () {
     var file = $(this).prop('files')[0];
-    $(this).closest('.p-file-button__selection').find('.file-name').text(file.name);
+    $(this).closest('.c-button__file--selection').find('.file-name').text(file.name);
    });
